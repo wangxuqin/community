452417a431a49aa742883a28cff5f0aa4684f5c1
kimwang <77255052@qq.com>
2019-02-28 11:47:45 +0800

    新增话题页面;

--- src/pages/topics/topics.ts
+++ src/pages/topics/topics.ts
@@ -14,7 +14,10 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'topics.html',
 })
 export class TopicsPage {
-
+  public titles:any = ["話題討論", "與我相關"];
+  public index:number = 1;
+  public topics:any = new Array(10);
+  
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
@@ -22,4 +25,47 @@ export class TopicsPage {
     console.log('ionViewDidLoad TopicsPage');
   }
 
+  public doTabChange(index)
+  {
+    this.index = index;
+  }
+
+  //下拉刷型界面
+  doRefresh(refresher){
+    setTimeout(()=>{
+      refresher.complete();
+    }, 2000);
+  }
+
+  public doInfinite(infiniteScroll){
+
+    setTimeout(() => {
+      infiniteScroll.complete();
+    },2000);
+
+    // //toast提示
+    // this.showInfo("加载成功");
+    // //增加index
+    // this.cnt ++;
+    // var data1 = {name:'yellowcong'+this.cnt,age:'1994', gender:'1', description:'三炮',img:"http://www.runoob.com/try/demo_source/venkman.jpg"};
+    // this.users.push(data1);
+
+
+    // if(this.cnt >=10){
+    //     //如果都加载完成的情况，就直接 disable ，移除下拉加载
+    //     infiniteScroll.enable(false);
+    //     //toast提示
+    //     this.showInfo("已加载所有");
+    // }
+  }
+
+  public toTop()
+  {
+    alert("返回頂部");
+  }
+
+  public toNewTopic()
+  {
+    alert("新增話題");
+  }
 }
