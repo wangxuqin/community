75766920553d4891b9b47d1ed54bf88f72b872f1
kimwang <77255052@qq.com>
2019-03-03 01:07:08 +0800

    新增safehtml管道;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { PipesModule } from '../pipes/pipes.module';
 import { MyApp } from './app.component';
 
 import { CounterInputComponent } from '../components/counter-input/counter-input';
 import { LabelTabsComponent } from '../components/label-tabs/label-tabs';
-
 
 import { HomePage } from '../pages/home/home';
 import { NewsPage } from '../pages/news/news';
@@ -36,6 +36,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
+    PipesModule,
     IonicModule.forRoot(MyApp, {
       iconMode:'ios',//  在整个应用程序中为所有图标使用的模式。可用选项："ios"，"md"
       mode:'ios'//在整个应用程序中使用的模式
